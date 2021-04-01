9b97eb33b415edcb5c39b321a5ff1f1d02a96688
Spencer <Spencer.Taber@yahoo.com>
2021-04-01 12:18:29 +0100

    Print list of uncompleted tasks

---
+++ task_list.py
@@ -0,0 +1,21 @@
+import pdb
+
+tasks = [
+    { "description": "Wash Dishes", "completed": False, "time_taken": 10 },
+    { "description": "Clean Windows", "completed": False, "time_taken": 15 },
+    { "description": "Make Dinner", "completed": True, "time_taken": 30 },
+    { "description": "Feed Cat", "completed": False, "time_taken": 5 },
+    { "description": "Walk Dog", "completed": True, "time_taken": 60 },
+]
+
+def uncompleted_tasks(list):
+    #breakpoint()
+    completed_false = []
+
+    for task in list:
+        if task["completed"] == False:
+          completed_false.append(task["description"])
+
+    return completed_false
+
+print(uncompleted_tasks(tasks))
